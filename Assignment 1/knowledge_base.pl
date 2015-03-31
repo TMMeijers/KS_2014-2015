7161f775c9106d235a7350bf8e42aedfb78a6bab
TMMeijers <t.m.meijers@hotmail.com>
2015-03-31 15:41:54 +0200

    i love klone

--- Assignment 1/knowledge_base.pl
+++ Assignment 1/knowledge_base.pl
@@ -1,113 +1,80 @@
-:- dynamic(descends_from/2).
+%% Animals
+descends_from(cold_blooded, animal).
+descends_from(warm_blooded, animal).
 
-%%% ANIMAL HIERARCHY
+descends_from(mammal, warm_blooded).
+descends_from(bird, warm_blooded).
+descends_from(reptile, cold_blooded).
 
-%% CHORDATA
-%% Phyla
-descends_from(chordata, animal).
-%% Classes
-descends_from(amphibie, chordata).
-descends_from(reptile, chordata).
-descends_from(bird, chordata).
-descends_from(mammal, chordata).
-descends_from(ostheichthye, chordata).
-%% Amphibie
-descends_from(amphibie, lizard).
-descends_from(amphibie, frog).
+descends_from(horse, mammal).
+descends_from(gorilla, mammal).
+descends_from(wolf, mammal).
+descends_from(whale, mammal).
+descends_from(human, mammal).
 
-%% Bird
-descends_from(pinguin, bird).
 descends_from(eagle, bird).
+descends_from(pigeon, bird).
+descends_from(pinguin, bird).
 
-%% Mammal
-descends_from(cangaroo, mammal).
-descends_from(rabbit, mammal).
-descends_from(dolphin, mammal).
-	      
-%% ARTHROPODA
-%% Phyla
-descends_from(arthropoda, animal).
-%% Classes
-descends_from(arachnid, arthropoda).
-descends_from(insect, arthropoda).
-descends_from(centipede, arthropoda).
-%% Arachnides
-descends_from(scorpion, arachnid).
-descends_from(spider, arachnid).
-descends_from(mite, arachnid).     
-%% Insects
+descends_from(crocodile, reptile).
+descends_from(snake, reptile).
+descends_from(lizard, reptile).
 
-%% Centipedes
+concept(animal).
+concept(warm_blooded).
+concept(cold_blooded).
+concept(mammal).
+concept(bird).
+concept(reptile).
 
-%%% OBJECT HIERARCHY
-descends_from(leg, body_part).
-descends_from(skeleton, body_part).
-descends_from(brain, body_part).
-descends_from(wings, body_part).
-descends_from(spine, body_part).
-descends_from(hair, body_part).
-descends_from(feathers, body_part).
+%% Living environment
 
-%% Segmented body
-descends_from(segmented_body, body).
-descends_from(body_segment, segmented_body).
+descends_from(walking, movement_type).
+descends_from(swimminmg, movement_type).
+descends_from(flying, movement_type).
 
-%% skeleton
-descends_from(exo_skeleton, skeleton).
-descends_from(endo_skeleton, skeleton).
+has_relation(animal, 1/inf, movement_type).
+has_relation(bird, 1/1, walking).
+has_relation(reptile, 1/1, walking).
+has_relation(horse, 1/1, walking).
+has_relation(gorilla, 1/1, walking).
+has_relation(wolf, 1/1, walking).
+has_relation(whale, 1/1, swimming).
+has_relation(eagle, 1/1, flying).
+has_relation(pigeon, 1/1, flying).
+has_relation(pinguin, 1/1, walking).
+has_relation(pinguin, 1/1, swimming).
+has_relation(crocodile, 1/1, swimming).
 
-%% brain
-descends_from(brain_with_neocortex, brain).
+%% Limbs
 
-%% KEY-VALUE-RESTRICTIONS
-% Mammals
-has_relation(cangaroo, 2/2, leg).
-has_relation(dolphin, 0/0, leg).
-has_relation(rabbit, 2/2, leg).
+descends_from(leg, limb).
+descends_from(arm, limb).
+descends_from(wing, limb).
+descends_from(fin, limb).
+descends_from(tail, limb).
+
+has_relation(animal, 0/inf, limb).
+has_relation(bird, 2/2, wing).
 has_relation(bird, 2/2, leg).
-has_relation(bird, 2/2, wings).
-has_relation(bird, 1/1, feathers).
-has_relation(bird, 0/0, hair).
+has_relation(bird, 1/1, tail).
+has_relation(horse, 4/4, leg).
+has_relation(horse, 1/1, tail).
+has_relation(gorilla, 2/2, leg).
+has_relation(gorilla, 2/2, arm).
+has_relation(wolf, 4/4, leg).
+has_relation(wolf, 1/1, tail).
+has_relation(whale, 2/2, fin).
+has_relation(human, 2/2, leg).
+has_relation(human, 2/2, arm).
+has_relation(crocodile, 4/4, leg).
+has_relation(crocodile, 1/1, tail).
+has_relation(snake, 0/0, limb).
+has_relation(lizard, 4/4, leg).
+has_relation(lizard, 1/1, tail).
 
-% Chordata
-has_relation(mammal, 1/1, brain_with_neocortex).
-has_relation(mammal, 1/1, hair).
-% HACK
-%has_relation(mammal, 0/0, feathers).
+%% Reproduction
 
-% Animal
-has_relation(chordata, 1/1, endo_skeleton).
-has_relation(chordata, 1/1, spine).
+descends_from(egg, reproduction).
+descends_from(birth, reproduction).
 
-% Arachnids
-has_relation(arachnid, 8/8, leg).
-has_relation(arachnid, 2/2, body_segment).
-
-% Insects
-has_relation(insect, 6/6, leg).
-has_relation(insect, 3/3, body_segment).
-
-% Centipedes
-has_relation(centipede, 20/300, leg).
-has_relation(centipede, 15/inf, body_segment).
-
-% Arthropoda
-has_relation(arthropoda, 1/1, exo_skeleton).
-has_relation(arthropoda, 1/1, segmented_body).
-has_relation(arthropoda, 0/inf, leg).
-has_relation(arthropoda, 0/0, spine).
-
-% Top Level
-has_relation(animal, 0/1, feathers).
-has_relation(animal, 0/1, hair).
-has_relation(animal, 0/2, wings).
-has_relation(animal, 0/1, spine).
-has_relation(animal, 0/inf, leg).
-has_relation(animal, 1/inf, body_segment).
-has_relation(animal, 1/1, brain).
-has_relation(animal, 1/1, skeleton).
-
-%% Adds an item to the knowledge base
-add_descends_from(X, Y) :-
-	X \= Y,
-	assert(descends_from(X, Y)).
